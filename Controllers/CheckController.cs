d43d6bcb13de9a0279e137ecc28fac9942533131
Linconl <67241771+LinconlG@users.noreply.github.com>
2023-09-01 16:34:00 -0300

    Create CheckController.cs

---
+++ Controllers/CheckController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TreinoSportAPI.Models;
+using TreinoSportAPI.Services;
+
+namespace TreinoSportAPI.Controllers {
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CheckController : Controller {
+
+
+        [HttpGet]
+        public ActionResult Check() {
+            try {
+                return Ok();
+            }
+            catch (Exception e) {
+                throw new Exception(e.Message, e.InnerException);
+            }
+        }
+    }
+}
